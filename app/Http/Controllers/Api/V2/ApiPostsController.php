8d02489e216b9f52a7acb1d318fe828d036eac1e
Bola <bolanaguib@gmail.com>
2025-06-18 11:02:14 +0300

    xx

--- app/Http/Controllers/Api/V2/ApiPostsController.php
+++ app/Http/Controllers/Api/V2/ApiPostsController.php
@@ -68,7 +68,7 @@ class ApiPostsController extends Controller
     public function update(Request $request, Post $post)
     {
         $data = $request->validate([
-            'slug'  => ['required', 'alpha_dash', 'unique:posts,slug'],
+            'slug'  => ['required', 'alpha_dash'],
             'category_id'    => ['nullable', 'exists:post_type_categories,id'],
             'content'        => ['nullable', 'array'],
             'blocks'         => ['nullable', 'array'],
