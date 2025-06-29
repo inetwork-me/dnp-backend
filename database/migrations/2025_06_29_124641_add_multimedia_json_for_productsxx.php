4e08b7b13f91bc88f507b4fa3d72ee021df4e600
Bola <bolanaguib@gmail.com>
2025-06-29 15:56:36 +0300

    xx

--- database/migrations/2025_06_29_124641_add_multimedia_json_for_productsxx.php
+++ database/migrations/2025_06_29_124641_add_multimedia_json_for_productsxx.php
@@ -15,8 +15,7 @@ return new class extends Migration
             //
             $table
                 ->json('multimedia')
-                ->nullable()
-                ->default('[]');
+                ->nullable();
         });
     }
 
