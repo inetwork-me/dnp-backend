6e8665a147c0487ab396a47cf8f3eb809ebbe5e9
Bola <bolanaguib@gmail.com>
2025-07-01 08:58:24 +0300

    xx

--- app/Http/Resources/V1/ProductMiniCollection.php
+++ app/Http/Resources/V1/ProductMiniCollection.php
@@ -33,7 +33,6 @@ class ProductMiniCollection extends ResourceCollection
                     'type' =>  $data->type,
                     'category'  => get_single_category($data->category_id),
                     'avg_rating' => round((float) $avgRating, 1),
-                    'packageDetails' => $data
 
 
 
