825bc83b9c28e391841311f28e8ff393f0460fae
Bola Naguib <bolanaguib@gmail.com>
2025-12-04 19:42:03 +0200

    expanded product detailes

--- app/Http/Resources/V1/PostCollection.php
+++ app/Http/Resources/V1/PostCollection.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources\V1;
 
 use App\Http\Resources\UserResource;
+use App\Models\Product;
 use Illuminate\Http\Resources\Json\ResourceCollection;
 
 class PostCollection extends ResourceCollection
@@ -49,7 +50,20 @@ class PostCollection extends ResourceCollection
             if (!empty($block['fields']) && is_array($block['fields'])) {
                 foreach ($block['fields'] as $field) {
                     if (isset($field['name'])) {
-                        $fields[$field['name']] = $field['value'] ?? null;
+                        $fieldValue = $field['value'] ?? null;
+
+                        // Expand product relations
+                        $isProductRelation = isset($field['relation_objectType']) && $field['relation_objectType'] === 'products';
+                        if ($isProductRelation && $fieldValue) {
+                            $fieldValue = $this->expandProductData($fieldValue);
+                        }
+
+                        // Handle repeater fields
+                        if (($field['type'] ?? '') === 'repeater' && is_array($fieldValue)) {
+                            $fieldValue = $this->expandRepeaterProducts($fieldValue, $field['fields'] ?? []);
+                        }
+
+                        $fields[$field['name']] = $fieldValue;
                     }
                 }
             }
@@ -62,6 +76,96 @@ class PostCollection extends ResourceCollection
         }
 
         return $blocks;
+    }
+
+    /**
+     * Expand product ID to full product data
+     */
+    protected function expandProductData($productId): ?array
+    {
+        if (!$productId) {
+            return null;
+        }
+
+        $product = Product::with(['product_translations', 'main_category', 'brand'])
+            ->find($productId);
+
+        if (!$product) {
+            return null;
+        }
+
+        $data = $product->toArray();
+        $data['main_price'] = $product->unit_price;
+
+        return $data;
+    }
+
+    /**
+     * Expand product relations within repeater fields
+     */
+    protected function expandRepeaterProducts(array $repeaterItems, array $fieldDefs): array
+    {
+        $productFieldNames = [];
+        foreach ($fieldDefs as $fieldDef) {
+            if (isset($fieldDef['relation_objectType']) && $fieldDef['relation_objectType'] === 'products') {
+                $productFieldNames[] = $fieldDef['name'];
+            }
+        }
+
+        $productIds = [];
+        $fieldsToExpand = [];
+
+        foreach ($repeaterItems as $item) {
+            foreach ($productFieldNames as $fieldName) {
+                if (isset($item[$fieldName]['value']) && $item[$fieldName]['value']) {
+                    $productIds[] = $item[$fieldName]['value'];
+                    $fieldsToExpand[$fieldName] = true;
+                } elseif (isset($item[$fieldName]) && is_numeric($item[$fieldName])) {
+                    $productIds[] = $item[$fieldName];
+                    $fieldsToExpand[$fieldName] = true;
+                }
+            }
+
+            // Scan for relation_objectType in item data
+            foreach ($item as $fieldName => $fieldData) {
+                if (is_array($fieldData) && isset($fieldData['relation_objectType']) && $fieldData['relation_objectType'] === 'products') {
+                    if (isset($fieldData['value']) && $fieldData['value']) {
+                        $productIds[] = $fieldData['value'];
+                        $fieldsToExpand[$fieldName] = true;
+                    }
+                }
+            }
+        }
+
+        if (empty($productIds)) {
+            return $repeaterItems;
+        }
+
+        $products = Product::with(['product_translations', 'main_category', 'brand'])
+            ->whereIn('id', array_unique($productIds))
+            ->get()
+            ->keyBy('id');
+
+        foreach ($repeaterItems as &$item) {
+            foreach (array_keys($fieldsToExpand) as $fieldName) {
+                $productId = null;
+
+                if (isset($item[$fieldName]['value']) && $item[$fieldName]['value']) {
+                    $productId = $item[$fieldName]['value'];
+                } elseif (isset($item[$fieldName]) && is_numeric($item[$fieldName])) {
+                    $productId = $item[$fieldName];
+                }
+
+                if ($productId && isset($products[$productId])) {
+                    $product = $products[$productId];
+                    $data = $product->toArray();
+                    $data['main_price'] = $product->unit_price;
+                    $item[$fieldName] = $data;
+                }
+            }
+        }
+
+        return $repeaterItems;
     }
 
     public function with($request)
